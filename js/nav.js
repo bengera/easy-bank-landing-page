5383a9b2aeb7cd5e69d0c0d4da847e30a77b7836
Ben <bengeraghty.bg@gmail.com>
2023-01-18 20:01:40 +0700

    add navigation-mobile

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+const hamMenu = document.querySelector('.hamburger-menu');
+// const popOutBox = document.querySelector('.nav__popout-box');
+
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    // popOutBox.classList.toggle('show');
+
+
+});
+
